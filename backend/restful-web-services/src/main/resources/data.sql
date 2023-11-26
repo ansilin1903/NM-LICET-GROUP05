ee62a483c5375b08696813f21d2be25f36044411
anurintha01 <124992147+anurintha01@users.noreply.github.com>
2023-11-26 10:44:13 +0530

    Delete backend directory

--- backend/restful-web-services/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into todo(id, username, task, target_date, done)
-values(10001, 'harshnagra', 'Learn JPA', sysdate(), false);
-
-insert into todo(id, username, task, target_date, done)
-values(10002, 'harshnagra', 'Integrate JWT', sysdate(), false);
-
-insert into todo(id, username, task, target_date, done)
-values(10003, 'harshnagra', 'Fix UI', sysdate(), false);
